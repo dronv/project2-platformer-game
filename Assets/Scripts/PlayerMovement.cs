d3b1d188791bc5f11ffbc2b0d2fcd46710a0af6d
carmen <carmen.werrlein@stud.th-deg.de>
2023-04-02 20:06:12 -0400

    some animations added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,6 @@
+using System.Xml.Serialization;
+using System.Runtime.CompilerServices;
+using System.IO.Pipes;
 // using System.ComponentModel.DataAnnotations.Schema;
 
 using System.Collections;
@@ -6,13 +9,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float moveInput = 0;
+    private float movementInput = 0;
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 15f;
 
     private SpriteRenderer sprite;
     private Rigidbody2D rb;
     private Animator anim;
+
+    private enum MovementState {_idle, _run, _jump, _fall};
     // Start is called before the first frame update 
     void Start()
     {
@@ -21,34 +26,47 @@ public class PlayerMovement : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();
     }
 
-
-    // Update is called once per frame
     void Update()
     {
-        moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        movementInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(movementInput * moveSpeed, rb.velocity.y);
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
-        updateAnimation();
+        updateAnimationState();
 
     }
 
-
-    private void updateAnimation()
+    private void updateAnimationState()
     {
-        if (moveInput > 0f)
+        MovementState movementState;
+
+        // check for 'run' input
+        if (movementInput > 0f)
         {
-            anim.SetBool("running", true);
+            movementState = MovementState._run;
             sprite.flipX = false;
         }
-        else if (moveInput < 0f)
+        else if (movementInput < 0f)
         {
-            anim.SetBool("running", true);
+            movementState = MovementState._run;
             sprite.flipX = true;
         }
-        else 
-        anim.SetBool("running", false);
+        else
+        { 
+            movementState = MovementState._idle;
+        }
+
+        // check for 'jump' input with tolerance
+        if (rb.velocity.y > .1f)
+        {
+            movementState = MovementState._jump;
+        } 
+        else if (rb.velocity.y < -.1f)
+        {
+            movementState = MovementState._fall;
+        }
+        anim.SetInteger("movementState", (int)movementState );
     }
 }
